c7a0efde7155a225121049d57da95e5e40854657
Fraz Shabbir <fraz.shabbir54@gmail.com>
2024-03-24 22:47:47 +0400

    project learning started

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello From FRAZ SHABBIR! I am a Software Engineer and I am learning React.js
         </p>
         <a
           className="App-link"
